db768f01d1fbe004bf9c946c041d7a9bf3fd8ece
Higuchi <lfmshiguchi@gmail.com>
2025-04-13 19:17:40 +0900

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styels/theme.css';
 import './styels/global.css';
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
 
       <Container>
-        <Heading>LOGO</Heading>
+        <Menu />
       </Container>
 
       <Container>
